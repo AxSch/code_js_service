b0eb5160eede1d7bde25235c5541adc112f95a3c
K.A. Schuneman <alex_schuneman@outlook.com>
2018-06-30 18:29:59 +0100

    feat: Added a route to return list of portfolios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,28 @@
 import log from "../logger";
+import db from "../database/index";
+import _ from 'lodash';
+
 
 export default (app) => {
   app.get('/', (req, res) => {
     log.info('/ called');
     res.json({ message: 'You Made it!' });
   });
+
+  app.get('/api/portfolios', (req, res) => {
+    const data = db.load();
+    const portArr = [];
+    const port = data.portfolios.map(item => {
+      const newObj =  _.cloneDeep(item);
+      newObj.positions = [];
+      data.positions.filter(item2 => {
+        if (item.id === item2.portfolioId) {
+          newObj.positions.push(item2);
+        }
+      });
+      portArr.push(newObj);
+    });
+
+    res.json({ portfolios: portArr });
+  })
 };
