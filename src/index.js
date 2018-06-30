652c0d3494882fa2fc4e3b8ad26418914ef4212c
K.A. Schuneman <alex_schuneman@outlook.com>
2018-06-30 17:06:28 +0100

    feat: Connected the server to DatabaseClient

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import http from 'http';
 import config from './config';
 import log from './logger';
 import routes from './routes';
+import DatabaseClient from "./database/index";
 
 const app = express();
 
@@ -12,6 +13,7 @@ const server = http.createServer(app);
 server.listen(config.port);
 server.on('error', error => log.error(error));
 server.on('listening', () => log.info(`Listening at ${config.url}`));
+const db = DatabaseClient.connect(server);
 
 process.on('uncaughtException', (error) => {
   log.error(error);
