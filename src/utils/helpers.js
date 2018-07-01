e417fa26bb462f8161c7be798885983a3a7599ec
K.A. Schuneman <alex_schuneman@outlook.com>
2018-07-01 15:15:46 +0100

    feat: Added three helpers to split up the request of creating a new entry

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import moment from 'moment';
 
 const structurePortf = (portfs, pos) => {
   let portArr = [];
@@ -15,7 +16,7 @@ const structurePortf = (portfs, pos) => {
   return portArr;
 }
 
-const filterOnCurrency = (pos, req)=> {
+const filterOnCurrency = (pos, req) => {
   let posArr = [];
   const portfs = pos.filter(item => {
     if (item.currency === req) {
@@ -25,8 +26,48 @@ const filterOnCurrency = (pos, req)=> {
   return posArr;
 }
 
+const countId = (dataArr) => {
+  let count = 0;
+  const ids = dataArr.map(item => {
+    if (item.id > count) {
+      count = item.id;
+    }
+  });
+  return count;
+}
+
+const createPortAndPos = (port, pos, req) => {
+  let newPortsArr = [];
+  const portCount = countId(port);
+  const posCount = countId(pos);
+  
+  const newPortfolio = {
+    id: portCount + 1,
+    name: req.body.name,
+  };
+  
+  const newPosition = {
+    id: posCount + 1,
+    portfolioId: portCount + 1,
+    currency: req.body.currency,
+    value: req.body.value,
+    date: moment().format('YYYY-MM-DD'),
+  };
+  
+  newPortsArr.push(newPortfolio);
+  newPortsArr.push(newPosition);
+  
+  return newPortsArr;
+}
+
+const addPortAndPos = (portArr, posArr, newPortfolio) => {
+  portArr.push(newPortfolio[0]);
+  posArr.push(newPortfolio[1]);
+}
 
 export {
   structurePortf,
   filterOnCurrency,
+  createPortAndPos,
+  addPortAndPos,
 };
